ca1967fef9445f435f103e96361885fb18845f3d
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2022-12-14 19:58:43 +0530

    multiple pages in SPA (react routers)

--- src/Components/Layout/NavBar/NavBar.js
+++ src/Components/Layout/NavBar/NavBar.js
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./NavBar.module.css";
 const NavBar = (props) => {
   return (
     <Fragment>
       <nav className={classes.navbar}>
-        <a className={classes.a} href="/">HOME</a>
-        <a className={classes.a}href="/store">STORE</a>
-        <a className={classes.a}href="/about">ABOUT</a>
+        <a className={classes.a}  href="/">HOME</a >
+        <a className={classes.a} href="/store">STORE</a >
+        <NavLink activeClassName={classes.a} to="/AboutUs">ABOUT</NavLink >
         
       </nav>
     </Fragment>
